66b7f1f5a3468e3a6f89bcca578a4c7f2e1a830f
benconway7 <126519619@umail.ucc.ie>
2026-09-19 15:57:12 +0100

    G to L

--- Name.py
+++ Name.py
@@ -1,3 +1,15 @@
 # My First Python program 
 name = input ("what is your name? ")
 print (f"hello, {name}! Welcome to Engineering Python.")
+
+# calculate the area of a circle
+radius = float(input("Enter the radius of the circle: "))
+area = 3.14159 * radius ** 2
+print(f"The area of the circle is: {area}") 
+
+# calculate the length of the hypotenuse of a right triangle
+import math
+a = float(input("Enter the length of side a: "))
+b = float(input("Enter the length of side b: "))
+c = math.hypot(a, b)
+print(f"The length of the hypotenuse is: {c}")
