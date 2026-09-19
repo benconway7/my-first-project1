eea05c547a008c296fa9f9ad6d189163ac6dc93d
benconway7 <126519619@umail.ucc.ie>
2026-09-19 16:06:25 +0100

    Angle converter

---
+++ Angle converter.py
@@ -0,0 +1,4 @@
+# Degrees to radians
+angle_in_degrees = float(input("Enter the angle in degrees: "))
+angle_in_radians = angle_in_degrees * (3.14159 / 180)
+print(f"The angle in radians is: {angle_in_radians:.4f} radians")
